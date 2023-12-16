59672bcac963d0d57f5aea32fc4ed7b8c6b9ffea
German_X <43143934+germankhamuliak@users.noreply.github.com>
2023-12-16 17:47:33 +0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import "./scripts/burger.js";
-import "./scripts/slider.js"
-import "./scripts/info2.js"
-import "./scripts/info3.js"
-import "./scripts/testimonials.js"
-import "./scripts/footer.js"
